6105c4fdb23417cfd4e391254912ae39a59bd626
Tiyo <eduardotiyo0@gmail.com>
2025-02-19 20:44:50 -0300

    feat: add initial restaurant pages and menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,45 @@
+import { ConsumptionMethod } from "@prisma/client";
+import {
+  ChevronLeftIcon,
+  ChevronsLeftIcon,
+  ScrollTextIcon,
+} from "lucide-react";
+import { iconNames } from "lucide-react/dynamic";
+import { ResponseLimit } from "next";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+
+import ConsumptionMethodOption from "../components/consumption-method-option";
+import RestaurantHeader from "./components/headers";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) return notFound();
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
