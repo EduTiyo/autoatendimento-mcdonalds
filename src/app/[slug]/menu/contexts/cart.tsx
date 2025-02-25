499f10ca32decda343e14f0ce3643f588db07624
EduTiyo <eduardotiyo0@gmail.com>
2025-02-25 12:10:53 -0300

    feat: bottom cart added if items greater than one

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -11,6 +11,7 @@ export interface ICartContext {
     isOpen: boolean;
     products: CartProduct[];
     total: number;
+    totalQuantity: number
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void;
     decreaseProductQuantity: (productId: string) => void;
@@ -23,6 +24,7 @@ export const CartContext = createContext<ICartContext>({
     isOpen: false,
     products: [],
     total: 0,
+    totalQuantity: 0,
     toggleCart: () => {},
     addProduct: () => {},
     decreaseProductQuantity: () => {},
@@ -41,6 +43,10 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
     const toggleCart = () => {
         setIsOpen(prev => !prev);
     };
+    const totalQuantity = products.reduce((acc, product) => {
+        return acc + product.quantity;  
+    },0);
+
     const addProduct = (product: CartProduct) => {
         const productIsAlreadyOnTheCart = products.some(prevProducts => prevProducts.id === product.id);
         if(!productIsAlreadyOnTheCart) {
@@ -91,6 +97,7 @@ export const CartProvider = ({children}: {children: ReactNode}) => {
             isOpen: isOpen,
             products: products,
             total,
+            totalQuantity,
             toggleCart: toggleCart,
             addProduct,
             decreaseProductQuantity,
