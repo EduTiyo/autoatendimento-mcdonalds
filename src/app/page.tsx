2d204f80d77a248396bc833e4b45d8cb8b6ef733
EduTiyo <eduardotiyo0@gmail.com>
2025-02-25 13:24:54 -0300

    feat: adding initial restaurants page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,46 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
-const HomePage = () => {
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+
+const getRestaurant = async () => {
+  return await db.restaurant.findFirst({
+    select: {
+      id: true,
+      name: true,
+      avatarImageUrl: true,
+      slug: true,
+    },
+  });
+};
+
+const HomePage = async () => {
+  const restaurant = await getRestaurant();
+
+  if (!restaurant) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-      <Button>Button</Button>
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex items-center p-5 justify-center flex-col">
+        <div className="mt-10">
+          <Image src={restaurant.avatarImageUrl} alt={restaurant.name} width={80} height={80}/>
+        </div>
+        <div className="flex mt-5 font-semibold text-xl">
+          {restaurant.name}
+        </div>
+        <div className="flex mt-5">
+          <Link href={`/${restaurant.slug}`}>
+            <Button variant="secondary">
+              Acessar
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
+
 export default HomePage;
